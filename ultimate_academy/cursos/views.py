b4fd27b74f69c24d3d5b44254e10d8cb16ddd3a2
Ivan Lezcano <lezcamati@gmail.com>
2024-07-11 15:25:49 -0300

    clases views

--- ultimate_academy/cursos/views.py
+++ ultimate_academy/cursos/views.py
@@ -7,6 +7,7 @@ def index(request):
     return render(request, 'ultimate_academy/index.html')
 
 def cursos(request):
+    '''Vista principal que retorna todos los cursos precargados'''
     # Traigo todos los Ids existentes en la BBDD
     ids = Video.objects.values_list('youtube_id', flat=True)
 
@@ -21,9 +22,9 @@ def cursos(request):
 
         video_data = {
             'titulo': video_info['snippet']['title'],
-            'duracion': video_info['contentDetails']['duration'],
             'fecha_publicacion': video_info['snippet']['publishedAt'],
             'miniatura': video_info['snippet']['thumbnails']['high']['url'],
+            'id': video_info['id'],
         }
         # Agrego el diccionario a la lista 'videos_info'
         videos_info.append(video_data)
@@ -37,20 +38,20 @@ def cursos(request):
 
     return render(request, 'ultimate_academy/cursos.html', context)
 
-def filtrar_duracion(parametro):
-    '''Funcion que toma como parametro un string dado de la API y modifica lo vista de la duracion del tiempo del video'''
-    duracion = ''
-    numbers = '0123456789'
-    for i in parametro:
-        if i in numbers:
-            duracion += i
-        elif i == 'H':
-            duracion += 'h:'
-        elif i == 'M':
-            duracion += 'm:'
-        elif i == 'S':
-            duracion += 's'
-        else: 
-            continue
-    
-    return duracion
+
+def clase(request, youtube_id):
+    data = api_youtube(youtube_id)
+    video_info = data['items'][0]
+
+    video_data = {
+        'titulo': video_info['snippet']['title'],
+        'duracion': video_info['contentDetails']['duration'],
+        'fecha_publicacion': video_info['snippet']['publishedAt'],
+        'miniatura': video_info['snippet']['thumbnails']['high']['url'],
+        'id': video_info['id'],
+    }
+
+    context = {
+        'clase': video_data,
+    }
+    return render(request, 'ultimate_academy/clase.html', context)
--- ultimate_academy/ultimate_academy/urls.py
+++ ultimate_academy/ultimate_academy/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from cursos.views import index, cursos
+from cursos.views import index, cursos, clase
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('cursos/', cursos, name='cursos'),
+    path('cursos/<str:youtube_id>/', clase, name='clase'),
 ]
