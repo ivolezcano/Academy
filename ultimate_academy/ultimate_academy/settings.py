0f5d7dcaf78e5c05d9beb83f2aef3dd993882b6c
Ivan Lezcano <lezcamati@gmail.com>
2024-07-31 20:15:42 -0300

    Debug False

--- ultimate_academy/ultimate_academy/settings.py
+++ ultimate_academy/ultimate_academy/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h)x^35!#5glaelj1p_2neiklj6@7iijs@g-eimoeh=7zb^^1#9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','.vercel.app', '.now.sh', 'localhost']
 
